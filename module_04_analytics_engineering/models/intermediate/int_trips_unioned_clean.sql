cc8b32a2f7a0ea19df3dc75486a83e564f4277f5
Lorenzo Ederone <ederonelorenzo@gmail.com>
2026-02-13 21:16:13 +0000

    Clean and create unique trip ID

--- module_04_analytics_engineering/models/intermediate/int_trips_unioned_clean.sql
+++ module_04_analytics_engineering/models/intermediate/int_trips_unioned_clean.sql
@@ -4,9 +4,10 @@ with int_trips_unioned as (
 )
 
 , add_uuid as (
-    select row_number() over (order by pickup_datetime, pickup_location_id, vendor_id, fare_amount) as trip_id,
+    select {{ dbt_utils.generate_surrogate_key(["vendor_id", "pickup_datetime", "pickup_location_id", "taxi_type"]) }} as trip_id,
            *
     from int_trips_unioned
 )
 
 select * from add_uuid
+qualify row_number() over (partition by vendor_id, pickup_datetime, pickup_location_id, taxi_type order by dropoff_datetime) = 1
