edc670faf1023974259657b75286d446e58748c3
Lorenzo Ederone <ederonelorenzo@gmail.com>
2026-02-13 18:50:06 +0000

    Create dim_vendors

--- module_04_analytics_engineering/models/marts/dim_vendors.sql
+++ module_04_analytics_engineering/models/marts/dim_vendors.sql
@@ -4,7 +4,8 @@ with trips_unioned as (
 )
 
 , vendors as (
-    select distinct vendor_id
+    select distinct vendor_id,
+           {{ get_vendor_names('vendor_id') }} as vendor_name
     from trips_unioned
 )
 
